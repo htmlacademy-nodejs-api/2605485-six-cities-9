e9f0a4bcea4f22d5f5cb76bc33d858731bdac6a9
SergeyMitkin <sergeymitkin@gmail.com>
2025-02-24 01:33:33 +0300

    Раскрашен вывод информации в консоли

--- src/cli/commands/help.command.ts
+++ src/cli/commands/help.command.ts
@@ -1,3 +1,4 @@
+import chalk from "chalk";
 import { Command } from './command.interface.js';
 
 export class HelpCommand implements Command {
@@ -6,15 +7,19 @@ export class HelpCommand implements Command {
   }
 
   public async execute(..._parameters: string[]): Promise<void> {
-    console.info(`
+    console.info(chalk.blue(`
         Программа для подготовки данных для REST API сервера.
-        Пример:
-            cli.js --<command> [--arguments]
-        Команды:
-            --version:                   # выводит номер версии
+        `),
+        chalk.green(`Пример:
+        `),
+            chalk.yellow(`cli.js --<command> [--arguments]
+        `),
+        chalk.green(`Команды:
+        `),
+            chalk.yellow(`--version:                   # выводит номер версии
             --help:                      # печатает этот текст
             --import <path>:             # импортирует данные из TSV
             --generate <n> <path> <url>  # генерирует произвольное количество тестовых данных
-    `);
+    `));
   }
 }
